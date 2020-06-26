3a849753d1a3c296e6e13fe3209247efb86812a8
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-06-26 12:03:44 +0200

    Switched for sync publishing

--- proxy-reports-notif.js
+++ proxy-reports-notif.js
@@ -1,4 +1,9 @@
 module.exports.handler = async (event) => {
-  // simply proxy event to SNS topic because SQS cannot post directly to SNS
-  return event.Records[0]
+  const AWS = require('aws-sdk')
+  const sns = new AWS.SNS({ apiVersion: '2010-03-31' })
+  const publishParams = {
+    Message: JSON.stringify(event.Records[0]),
+    TopicArn: 'arn:aws:sns:eu-central-1:374163881813:mws-report-topic'
+  }
+  return await sns.publish(publishParams).promise().then(res => true).catch(res => false)
 }
