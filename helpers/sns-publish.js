e690c4858636bff2559f94e307fac6d57edd010d
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-06-26 16:15:14 +0200

    Added extra console logging for monitoring

--- helpers/sns-publish.js
+++ helpers/sns-publish.js
@@ -1,4 +1,6 @@
 module.exports = ({ msg, reportType }) => {
+  console.log(JSON.stringify(msg, null, 2))
+  console.log(JSON.stringify(reportType, null, 2))
   const AWS = require('aws-sdk')
   const sns = new AWS.SNS({ apiVersion: '2010-03-31' })
   const publishParams = {
